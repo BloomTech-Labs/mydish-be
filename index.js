4ba6d1f0d2ca2b42b74988c1c5dfae8b4c6ddab4
yurika sakae <yurika.sun@gmail.com>
2019-10-16 14:29:39 -0700

    initial commit, server setup'

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+const server = require("./server.js");
+
+const port = process.env.PORT || 4444;
+server.listen(port, () => console.log(`API running on port: ${port}`));
