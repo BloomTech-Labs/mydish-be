42b92cdb049eec96d3f47668242d199cc05e72de
antilou86 <antilou86@gmail.com>
2019-10-28 15:27:52 -0400

    added routing in server file.

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ server.use(express.json());
 //server.use('/foos', fooRouter);
 const CooksRouter = require("./routers/cooks.js");
 const RecipeRouter = require("./routers/recipes.js");
+const CookbookRouter = require("./routers/cookbook.js")
 
 server.use("/cooks", CooksRouter);
 server.use("/recipes", RecipeRouter);
+server.use("/cookbook", CookbookRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ hello: "world", recieved: req.body });
