37c8e6a245bc447df3375f4ce321465920acefdd
antilou86 <antilou86@gmail.com>
2019-11-22 11:46:54 -0500

    added endpoint for edits table where FE can grab recipe details for all immediate children of a given recipe.

--- routers/edits.js
+++ routers/edits.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const Recipes = require('../data/recipeModel.js');
 const Edits = require('../data/editsModel.js');
 
 //all edits by ancestor recipe id, only grabs immediate children.
 //future releases may require recursive db calls to find children of children.
-
+//i have this under a /dev/ because i am testing it out. until it's working just use /:id
 router.get('/:id', (req, res) => {
-    Edits.recipesByAncestorId(req.params.id)
+    Edits.searchByAncestor(req.params.id)
     .then(dbRes => {
         res.status(201).json({
             message: 'Recipes found',
@@ -22,3 +21,21 @@ router.get('/:id', (req, res) => {
     })
 });
 
+// //all edits by ancestor recipe id, only grabs immediate children, only returns an array of child recipe I.
+// router.get('/test/:id', (req, res) => {
+//     Edits.findAllByAncestorId(req.params.id)
+//     .then(dbRes => {
+//         res.status(201).json({
+//             message: 'Recipes found',
+//             data: dbRes
+//         })
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(501).Json({
+//             message:"there was an error with your request",
+//             error: err
+//         });
+//     })
+// })
+module.exports = router;
