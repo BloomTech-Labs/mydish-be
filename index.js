3984a0b7e73af95e25ff7628ca45c9f51f5ce47f
dustinsnoap <afrobacon@gmail.com>
2019-12-21 12:49:45 -0500

    combined server and index

--- index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const server = require('./server.js')
-const port = process.env.PORT || 4447
-
-server.listen(port, () => {
-    console.clear()
-    console.log(`\n*** Go ahead, make my port ${port} **\n`)
-})
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
 //setup
 const express = require('express')
 const server = express()
 const router = express.Router()
+const port = process.env.PORT || 4447
 
 //import and use middleware
 server.use(require('cors')())
@@ -9,10 +12,16 @@ server.use(require('helmet')())
 server.use(express.json())
 
 //routers
-server.use('/users', require('./endpoints/users.js'))
-server.use('/recipes', require('./endpoints/recipes.js'))
+// server.use('/users', require('./endpoints/users.js'))
+// server.use('/recipes', require('./endpoints/recipes.js'))
+
+//signal that the server is in fact running
+server.listen(port, () => {
+    console.clear()
+    console.log(`\n*** Go ahead, make my port ${port} **\n`)
+})
 
 //catchall endpoint
 server.get('/', (req, res) => {
-    res.status(200).json('Yup, it works.')
+    res.status(200).json('Yup, it worksing.')
 })
