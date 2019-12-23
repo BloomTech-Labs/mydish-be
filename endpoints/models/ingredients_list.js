bc8a4377848d889646881208c2acc1668de05eb7
dustinsnoap <afrobacon@gmail.com>
2019-12-22 21:20:25 -0500

    added ingredients_list model

---
+++ endpoints/models/ingredients_list.js
@@ -0,0 +1,29 @@
+const db = require('../../data/dbConfig')
+const tbl = 'ingredients_list'
+
+add_one = async (obj) => 
+    (await db(tbl).insert(obj).returning('*'))[0]
+
+get_one = async (search_params) =>
+    await db(tbl).where(search_params).first()
+
+get_all = async (search_params = {}) =>
+    await db(tbl).where(search_params)
+
+update_one = async (id, obj) =>
+    (await db(tbl).where({id}).update(obj).returning('*'))[0]
+
+remove_one = async (id) =>
+    (await db(tbl).where({id}).delete().returning('*'))[0]
+
+remove_all = async () =>
+    await db(tbl).delete()
+
+module.exports = {
+    add_one,
+    get_one,
+    get_all,
+    update_one,
+    remove_one,
+    remove_all
+}
