8d306a2f5362e92ecda1ed76a6f70c2d3088e888
jambis <imjaamees@gmail.com>
2020-01-13 12:40:09 -0600

    Added knex-cleaner

--- data/seeds/006-savesData.js
+++ data/seeds/006-savesData.js
@@ -1,19 +1,12 @@
 exports.seed = function(knex) {
-  // Deletes ALL existing entries.
-  return knex("saves")
-    .truncate()
-    .then(function() {
-      // Inserts seed entries
-      return knex("saves").insert([
-        { cook_id: 1, recipe_id: 2 },
-        { cook_id: 1, recipe_id: 4 },
-        { cook_id: 2, recipe_id: 1 },
-        { cook_id: 2, recipe_id: 2 },
-        { cook_id: 2, recipe_id: 3 },
-        { cook_id: 2, recipe_id: 4 },
-        { cook_id: 3, recipe_id: 2 },
-        { cook_id: 3, recipe_id: 3 }
-      ]);
-    });
+  return knex("saves").insert([
+    { cook_id: 1, recipe_id: 2 },
+    { cook_id: 1, recipe_id: 4 },
+    { cook_id: 2, recipe_id: 1 },
+    { cook_id: 2, recipe_id: 2 },
+    { cook_id: 2, recipe_id: 3 },
+    { cook_id: 2, recipe_id: 4 },
+    { cook_id: 3, recipe_id: 2 },
+    { cook_id: 3, recipe_id: 3 }
+  ]);
 };
-//revert
