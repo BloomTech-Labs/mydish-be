90e2622afde6b3182e8420838d4db16e4e765866
Devin Warrick <dev.warr@gmail.com>
2020-01-15 13:25:34 -0600

    Add testing config options

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,42 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     }
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
+    }
+  },
+  testing: {
+    client: "pg",
+    connection: process.env.DATABASE_TEST_URL || {
+      database: process.env.DB_TEST_NAME,
+      user: process.env.DB_TEST_USER,
+      password: process.env.DB_TEST_PASSWORD
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
-}
+};
