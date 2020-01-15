7ce0142e11a45bde6623649ab71dc4b19e2cb308
Devin Warrick <dev.warr@gmail.com>
2020-01-15 12:13:55 -0600

    Updates models to account for new db

---
+++ endpoints/models/ingredients_strings_only.js
@@ -0,0 +1,49 @@
+const db = require('../../data/dbConfig')
+const tbl = 'ingredients_strings_only'
+
+add_one = async (obj) => 
+    (await db(tbl).insert(obj).returning('*'))[0]
+
+get_one = async (search_params) => 
+    await db(`${tbl} as list`)
+        .where('list.id', '=', search_params.id)
+        .join('ingredients', {'list.ingredient_id': 'ingredients.id'})
+        .join('units', {'list.unit_id': 'units.id'})
+        .select(
+            'list.recipe_id',
+            'ingredients.name as ingredient',
+            'units.name as unit',
+            'units.abbreviation as unit_short',
+            'list.quantity'
+        )
+        .first()
+
+get_all = async (search_params = {}) =>
+    await db(`${tbl} as list`)
+        .join('ingredients', {'list.ingredient_id': 'ingredients.id'})
+        .join('units', {'list.unit_id': 'units.id'})
+        .select(
+            'list.recipe_id',
+            'ingredients.name as ingredient',
+            'units.name as unit',
+            'units.abbreviation as unit_short',
+            'list.quantity'
+        )
+
+update_one = async (id, obj) =>
+    (await db(tbl).where({id}).update(obj).returning('*'))[0]
+
+remove_one = async (id) =>
+    (await db(tbl).where({id}).delete().returning('*'))[0]
+
+remove_all = async () =>
+    await db(tbl).delete()
+
+module.exports = {
+    add_one,
+    get_one,
+    get_all,
+    update_one,
+    remove_one,
+    remove_all
+}
