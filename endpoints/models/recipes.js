6407a8140465750ab3615c2acdd538e9f5c8b4c8
Devin Warrick <dev.warr@gmail.com>
2020-01-15 18:16:16 -0600

    Refactors functions to have {} or to be one-liners, removes async syntax if unneeded

--- endpoints/models/recipes.js
+++ endpoints/models/recipes.js
@@ -60,8 +60,8 @@ add_one = async (obj) => {
     // return (await db(tbl).insert(obj).returning('*'))[0]
 }
 
-get_one = async (search_params) =>
-    await db(`${tbl} as r`)
+get_one = search_params => {
+    return db(`${tbl} as r`)
         .where({'r.id': search_params.id})
         .join('users', {'r.owner_id': 'users.id'})
         .join('recipe_ingredients as list', {'list.recipe_id': 'r.id'})
@@ -72,7 +72,7 @@ get_one = async (search_params) =>
             'r.id',
             'r.title',
             'r.description',
-            'r.forked_from as history',
+            'r.forked_from',
             db.raw(`json_build_object(
                 'user_id', users.id,
                 'username', users.username
@@ -92,9 +92,10 @@ get_one = async (search_params) =>
         )
         .groupBy('r.id', 'users.id')
         .first()
+}
 
-get_all = title =>
-    db(`${tbl} as r`)
+get_all = title => {
+    return db(`${tbl} as r`)
         // ilike = fuzzy search, ignore case
         // WHERE r.title LIKE %cerea%
         .where('r.title', 'ilike', `%${title}%`)
@@ -103,56 +104,20 @@ get_all = title =>
             'r.id',
             'r.title',
             'r.description',
-            'r.forked_from as history',
+            'r.forked_from',
             db.raw(`json_build_object(
                 'user_id', users.id,
                 'username', users.username
                 ) as owner`),
         )
         .groupBy('r.id', 'users.id')
+}
 
-// Older version of function
-get_all_LEGACY = async (search_params = {}) =>
-    await db(`${tbl} as r`).where(search_params)
-        .join('users', {'r.owner_id': 'users.id'})
-        .join('recipe_ingredients as list', {'list.recipe_id': 'r.id'})
-        .join('ingredients', {'list.ingredient_id': 'ingredients.id'})
-        .join('units', {'list.unit_id': 'units.id'})
-        .join('instructions', {'instructions.recipe_id': 'r.id'})
-        .select(
-            'r.id',
-            'r.title',
-            'r.description',
-            db.raw(`json_build_object(
-                'user_id', users.id,
-                'username', users.username
-                ) as owner`),
-            db.raw(`json_build_object(
-                'forked_from', r.forked_from
-                ) as history`),
-            db.raw(`json_agg(distinct jsonb_build_object(
-                'ingredient_list_id', list.id,
-                'ingredient', ingredients.name,
-                'quantity', list.quantity,
-                'units', units.name,
-                'units_short', units.abbreviation
-                )) as ingredients`),
-            db.raw(`json_agg(distinct jsonb_build_object(
-                'instruction_id', instructions.id,
-                'step', instructions.step_number,
-                'instruction', instructions.description
-                )) as instructions`)
-        )
-        .groupBy('r.id', 'users.id')
+update_one = (id, obj) => db(tbl).where({id}).update(obj).returning('*')[0]
 
-update_one = async (id, obj) =>
-    (await db(tbl).where({id}).update(obj).returning('*'))[0]
+remove_one = (id) => db(tbl).where({id}).delete().returning('*')[0]
 
-remove_one = async (id) =>
-    (await db(tbl).where({id}).delete().returning('*'))[0]
-
-remove_all = async () =>
-    await db(tbl).delete()
+remove_all = () => db(tbl).delete()
 
 module.exports = {
     add_one,
