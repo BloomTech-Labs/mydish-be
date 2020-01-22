1a15951fbbe9b646f16d135ca0921d8137f807f9
Devin Warrick <dev.warr@gmail.com>
2020-01-22 13:08:25 -0600

    Refactors password to a stand-alone-function

--- endpoints/middleware/encrypt.js
+++ endpoints/middleware/encrypt.js
@@ -1,10 +1,7 @@
 const crypt = require('bcryptjs')
 const password_strength = require('../../config/settings').password_strength
 
-password = async (req, res, next) => {
-    req.body.password = crypt.hashSync(req.body.password, password_strength)
-    next()
-}
+password = password => crypt.hashSync(password, password_strength)
 
 module.exports = {
     password
